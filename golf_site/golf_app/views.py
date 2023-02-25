ee64aa475da4397acd5721cc37d35c3657943693
henryklinck <henry.klinck@mail.utoronto.ca>
2023-02-24 23:27:07 -0500

    blog functionality functionality finished

--- golf_site/golf_app/views.py
+++ golf_site/golf_app/views.py
@@ -1,5 +1,6 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
+from .models import BlogPost
 
 # Create your views here.
 
@@ -7,3 +8,12 @@ def index(request):
 
     # return HttpResponse("<h1> Text Homepage <h1>")
     return render(request, 'base.html')
+
+def blog(request):
+
+    posts = BlogPost.objects.order_by('-timestamp') [0:3]
+    page_content = {
+        'posts': posts,
+    }
+
+    return render(request, 'blog.html', page_content)
