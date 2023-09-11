633ddca698d099c23f0fe970a050c6e7ecdcdb80
Henry Klinck <hklinck33@icloud.com>
2023-09-11 15:28:14 -0400

    updated static root

--- golf_site/golf_site/settings.py
+++ golf_site/golf_site/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'golf_site/static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'golf_site/static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
